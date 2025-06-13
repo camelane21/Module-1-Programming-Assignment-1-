8dc0cdf9088b392b543b43a46fac1bafbf6b739c
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-13 00:09:55 -0400

    Update Exercise06_09.java
    change

--- Exercise06_09.java
+++ Exercise06_09.java
@@ -9,3 +9,24 @@ public class Exercise06_09 {
     public static double meterToFoot(double meter) {
         return 3.279 * meter;
     }
+
+    public static void main(String[] args) {
+        // Print the table header
+        System.out.printf("%-10s%-10s    %-10s%-10s%n",
+                          "Feet", "Meters", "Meters", "Feet");
+        System.out.printf("%-10s%-10s    %-10s%-10s%n",
+                          "------", "------", "------", "------");
+
+        double meterColumn = 20.0;
+        for (double foot = 1.0; foot <= 10.0; foot++) {
+            double meters    = footToMeter(foot);
+            double feetBack  = meterToFoot(meterColumn);
+
+            System.out.printf("%-10.1f%-10.3f    %-10.1f%-10.3f%n",
+                              foot,      meters,
+                              meterColumn, feetBack);
+
+            meterColumn += 5.0;
+        }
+    }
+}
