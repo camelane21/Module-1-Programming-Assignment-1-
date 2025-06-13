45637b9c9ca789b9de7cbaf0af45e4bc077ef53a
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-12 23:20:58 -0400

    Create Exercise06_09.java
    footmeter

---
+++ Exercise06_09.java
@@ -0,0 +1,11 @@
+public class Exercise06_09 {
+
+    /** Convert from feet to meters */
+    public static double footToMeter(double foot) {
+        return 0.305 * foot;
+    }
+
+    /** Convert from meters to feet */
+    public static double meterToFoot(double meter) {
+        return 3.279 * meter;
+    }
